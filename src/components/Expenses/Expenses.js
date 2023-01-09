130e4500a7bff8510a7751640c5a53244201a93a
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-01-09 12:17:58 -0800

    Dynamic table to show expenses and filter by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,29 +13,26 @@ function Expenses(props){
         //console.log(selectedYear);
     };
 
+    //filter() return a new area with the items that returned true
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    //Create a Dynamic array
     return ( 
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
+            <ExpensesFilter 
+                selected={filteredYear} 
+                onChangeFilter={filterChangeHandler}
             />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
+            {filteredExpenses.map((expense) => ( 
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))}
         </Card>
     );
 }
