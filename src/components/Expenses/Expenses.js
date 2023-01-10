188fff4621045a7afab6fdd04f31d2f8a188ea9f
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-01-10 14:12:21 -0800

    Expense Bar Chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import './Expenses.css'
+
 
 function Expenses(props){
     const [filteredYear, setfilteredYear] = useState('2020')
@@ -25,14 +27,8 @@ function Expenses(props){
                 selected={filteredYear} 
                 onChangeFilter={filterChangeHandler}
             />
-            {filteredExpenses.map((expense) => ( 
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))}
+            <ExpensesChart expenses={filteredExpenses}/>
+            <ExpensesList items = {filteredExpenses}/>
         </Card>
     );
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 function NewExpense(props){
+    const [isEditing, setIsEditing] = useState(false);
+
     function onSaveExpenseDataHandler(enteredExpenseData){ //funtion that passe data from child to parent
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
+
+    function startEditingHandler(){
+        setIsEditing(true);
+    }
+
+    function stopEditingHandler() {
+        setIsEditing(false);
+    }
 
     return(
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && 
+            <ExpenseForm 
+                onSaveExpenseData={onSaveExpenseDataHandler} 
+                onCancel={stopEditingHandler} />
+            }
         </div>
     );
 };
